574f5221e909ffa625f45e21cb96e6f0d41c30b2
Ram <r.m.c.f.789@gmail.com>
2022-01-30 18:26:26 +0900

    Add:カメラ起動時の操作に条件分岐追加

--- app/javascript/packs/components/BeerTop.jsx
+++ app/javascript/packs/components/BeerTop.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import ReplayIcon from "@material-ui/icons/Replay";
 import { IconButton } from "@material-ui/core";
 import { useRef, useState, useCallback } from "react";
 import Webcam from "react-webcam";
 import Grid from "@material-ui/core/Grid";
-
-const videoConstraints = {
-  width: 380,
-  height: 460,
-  facingMode: "user",
-};
 
 const BeerTop = () => {
   const [isCaptureEnable, setCaptureEnable] = useState(false);
@@ -21,6 +16,49 @@ const BeerTop = () => {
       setUrl(imageSrc);
     }
   }, [webcamRef]);
+
+  const displayNone = () => {
+    let display = document.getElementsByClassName("wrapper")[0];
+    display.classList.add("display-none");
+  }
+  const displayShow = () => {
+    let display = document.getElementsByClassName("wrapper")[0];
+    display.classList.remove("display-none");
+  }
+
+  const CameraButton = () => {
+    if (isCaptureEnable) {
+      // 撮影
+      return (
+        <IconButton onClick={() => {
+          capture();
+          setCaptureEnable(false);
+        }}>
+          <CameraAltIcon />
+        </IconButton>
+      );
+    } else if (url) {
+      // 撮り直し
+      return (
+        <IconButton onClick={() => {
+          setUrl(null);
+          setCaptureEnable(true);
+        }}>
+          <ReplayIcon />
+        </IconButton>
+      );
+    } else {
+      // カメラ起動
+      return (
+        <IconButton onClick={() => {
+          displayNone();
+          setCaptureEnable(true);
+        }}>
+          <CameraAltIcon />
+        </IconButton>
+      );
+    }
+  }
 
   return (
     <>
@@ -51,67 +89,65 @@ const BeerTop = () => {
           <div className="beer-genre" id="genre-hint">
             <p>ビアスタイルとは？</p>
           </div>
-
-          {isCaptureEnable && (
-            <>
-              <div>
-                <Webcam
-                  audio={false}
-                  width={380}
-                  height={460}
-                  ref={webcamRef}
-                  screenshotFormat="image/jpeg"
-                  videoConstraints={videoConstraints}
-                />
-              </div>
-            </>
-          )}
-          {url && (
-            <>
-              <div>
-                <img src={url} alt="Screenshot" />
-              </div>
-            </>
-          )}
         </div>
       </div>
+
+      {/* カメラ起動時のディスプレイ */}
+      {isCaptureEnable && (
+        <div>
+          <Webcam
+            audio={false}
+            width={380}
+            height={460}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={{
+              width: 380,
+              height: 460,
+              facingMode: "user"
+            }}
+          />
+        </div>
+      )}
+      {url && (
+        <div>
+          <img src={url} alt="Screenshot" />
+        </div>
+      )}
 
       <div className="footer fixed beer-footer">
         <Grid container alignItems="center">
           <Grid item xs={4}>
             {/* カメラ停止 */}
             {isCaptureEnable && (
-              <span style={{ color: "#FFF", cursor: "pointer" }} onClick={() => setCaptureEnable(false)}>
+              <span style={{ color: "#FFF", cursor: "pointer" }} onClick={() => {
+                displayShow();
+                setCaptureEnable(false);
+              }}>
                 キャンセル
               </span>
             )}
             {/* 写真削除 */}
             {url && (
-              <span style={{ color: "#FFF", cursor: "pointer" }} onClick={() => setUrl(null)}>
+              <span style={{ color: "#FFF", cursor: "pointer" }} onClick={() => {
+                displayShow();
+                setUrl(null);
+              }}>
                 キャンセル
               </span>
             )}
           </Grid>
 
           <Grid item xs={4}>
-            {/* カメラ起動 */}
-            {isCaptureEnable || (
-              <IconButton onClick={() => setCaptureEnable(true)}>
-                <CameraAltIcon />
-              </IconButton>
-            )}
-            {/* 撮影&カメラ停止 */}
-            {isCaptureEnable && (
-              <IconButton onClick={() => {
-                capture();
-                setCaptureEnable(false);
-              }}>
-                <CameraAltIcon />
-              </IconButton>
-            )}
+            <CameraButton />
           </Grid>
 
           <Grid item xs={4}>
+            {url && (
+              <span>
+                <a href="/beel/image_seach" style={{ color: "#FFF", cursor: "pointer" }}>検索</a>
+              </span>
+            )}
           </Grid>
         </Grid>
       </div>
