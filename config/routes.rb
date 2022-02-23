968f96b3a1d97ada0af5f637ed33baed894dcf70
Ram <r.m.c.f.789@gmail.com>
2022-02-23 16:36:59 +0900

    Add:画像検索のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
+  post "/beers/image_seach" => "beers#image_seach"
   get "/beer" => redirect("/")
   get "/wine" => redirect("/")
   get "/sake" => redirect("/")
+
 end
