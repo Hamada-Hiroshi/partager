b6082af5a586e3cd8744f1d39820b3ca92df473a
Ram <r.m.c.f.789@gmail.com>
2022-03-05 16:05:50 +0900

    countriesテーブルにen_nameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_052054) do
+ActiveRecord::Schema.define(version: 2022_03_05_060451) do
 
   create_table "beer_styles", id: :integer, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_03_05_052054) do
 
   create_table "countries", id: :integer, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
+    t.string "en_name", null: false
     t.string "abbreviation", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
