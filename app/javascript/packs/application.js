4ad499705d464df9cf9c3caf75217b9b8e228773
Ram <r.m.c.f.789@gmail.com>
2022-05-03 16:50:18 +0900

    Fix:現時点で不要な記述をコメントアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,9 @@ import "bootstrap"
 Rails.start()
 ActiveStorage.start()
 
-const images = require.context("../images", true);
-const imagePath = (name) => images(name, true);
+// const images = require.context("../images", true);
+// const imagePath = (name) => images(name, true);
+
 const setHeight = () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
