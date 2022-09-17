54b837193033cd5ba6830e4083f43803965ea2d7
Ram <r.m.c.f.789@gmail.com>
2022-09-17 17:25:56 +0900

    Add:検索フォーム追加

--- app/javascript/packs/components/BeerTop.jsx
+++ app/javascript/packs/components/BeerTop.jsx
@@ -1,37 +1,90 @@
-import React from "react";
-import { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
+import { TextField } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import CameraSearch from "./CameraSearch";
 
 const BeerTop = () => {
+  const handleSearch = () => {
+    console.log("検索実行");
+    // setProgress(true);
+    // let csrfToken = document.head.querySelector("[name=csrf-token][content]").content;
+    // let data = {
+    //   "review": {
+    //     "drink_id": drinkId,
+    //     "drink_type": drinkType.charAt(0).toUpperCase() + drinkType.slice(1),
+    //     "score": rating,
+    //     "comment": inputComment.current.value
+    //   }
+    // }
+    //
+    // axios
+    //   .post("/reviews", data, {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "X-CSRF-Token": csrfToken
+    //     }
+    //   })
+    //   .then((response) => {
+    //     setReviewModalOpen(false);
+    //     displaySendButton(false);
+    //     setUserInfo({
+    //       isLogin: userInfo.isLogin,
+    //       reviewedBeerIds: userInfo.reviewedBeerIds.concat(drinkId),
+    //       reviewedWineIds: userInfo.reviewedWineIds,
+    //       reviewedSakeIds: userInfo.reviewedSakeIds
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   })
+    //   .finally(() => {
+    //     setProgress(false);
+    //   });
+  }
+
   return (
     <>
-      <div className="wrapper beer top">
-        <h2 className="sub-title">ビアスタイルで探す</h2>
-        <div id="select-beer-genre">
-          <Link to="/beers?category=lager" className="beer-genre" id="lager">
-            <p style={{ color: "#000" }}>
-              <span className="main-font">Lager</span>
-              <br />
-              <span className="sub-font">-ラガー-</span>
-            </p>
-          </Link>
-          <Link to="/beers?category=ale" className="beer-genre" id="ale">
-            <p style={{ color: "#FFF" }}>
-              <span className="main-font">Ale</span>
-              <br />
-              <span className="sub-font">-エール-</span>
-            </p>
-          </Link>
-          <Link to="/beers?category=others" className="beer-genre" id="others">
-            <p style={{ color: "#000" }}>
-              <span className="main-font">Others</span>
-              <br />
-              <span className="sub-font">-その他-</span>
-            </p>
-          </Link>
-          <div className="beer-genre" id="genre-hint">
-            <p>ビアスタイルとは？</p>
+      <div className="no-wrapper beer top">
+        <div className="beer-search">
+          <div id="beer-keyword-search">
+            <TextField
+              id="outlined-search"
+              label={false}
+              type="search"
+              variant="outlined"
+              placeholder="キーワードを入力してください"
+              fullWidth
+            />
+            <SearchIcon onClick={handleSearch} />
+          </div>
+
+          <h2 className="sub-title">ビールを検索する</h2>
+          <div id="select-beer-genre">
+            <Link to="/beers?category=lager" className="beer-genre" id="lager">
+              <p style={{ color: "#000" }}>
+                <span className="main-font">Lager</span>
+                <br />
+                <span className="sub-font">-ラガー-</span>
+              </p>
+            </Link>
+            <Link to="/beers?category=ale" className="beer-genre" id="ale">
+              <p style={{ color: "#FFF" }}>
+                <span className="main-font">Ale</span>
+                <br />
+                <span className="sub-font">-エール-</span>
+              </p>
+            </Link>
+            <Link to="/beers?category=others" className="beer-genre" id="others">
+              <p style={{ color: "#000" }}>
+                <span className="main-font">Others</span>
+                <br />
+                <span className="sub-font">-その他-</span>
+              </p>
+            </Link>
+            <div className="beer-genre" id="all-style">
+              <p>すべてのスタイル</p>
+            </div>
           </div>
         </div>
       </div>
